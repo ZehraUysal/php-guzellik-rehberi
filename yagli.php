59f0bb07b05a1f4c9fdebbc626cc346568e76a06
ZehraUysal <zehrauysal3558@gmail.com>
2025-06-17 23:53:30 +0300

    Update yagli.php

--- yagli.php
+++ yagli.php
@@ -5,10 +5,10 @@ require_once "connectDb.php" ;
 $sql = "SELECT * FROM cilt_onerileri WHERE cilt_tipi = 'yağlı'";
 $result = $conn->query($sql);
 
-// Saç tiplerine göre önerileri depolamak için boş diziler
+
 $yagli_cilt = [];
 
-// Eğer veri varsa, her bir satırı kontrol et ve ilgili dizilere yerleştir
+
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $yagli_cilt[] = $row;
