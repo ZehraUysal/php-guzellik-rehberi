0633b5fc6ecca7e6bd01abfc44b7e453a593b450
ZehraUysal <zehrauysal3558@gmail.com>
2025-06-17 23:52:43 +0300

    Update kuru.php

--- kuru.php
+++ kuru.php
@@ -4,10 +4,10 @@ require_once "connectDb.php" ;
 $sql = "SELECT * FROM cilt_onerileri WHERE cilt_tipi = 'kuru'";
 $result = $conn->query($sql);
 
-// Saç tiplerine göre önerileri depolamak için boş diziler
+
 $kuru_cilt = [];
 
-// Eğer veri varsa, her bir satırı kontrol et ve ilgili dizilere yerleştir
+
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $kuru_cilt[] = $row;
